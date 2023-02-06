a85578f83385c24cbab079776995c3c0105009bf
César Rincón <90701896+CesarRincon@users.noreply.github.com>
2023-02-06 10:50:57 -0500

    style: Added font styles

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,31 +1,34 @@
 import React from "react"
 import PropTypes from 'prop-types'
 
+
 type Props = {
     logo: string
     phone: string
     message: string
     height: number
     width: number
+    font: string
 }
 
-const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
+const WhatsappButton = ({ logo, phone, message, width, height, font }: Props) => {
 
     const formatTextMessage = message.replace(/ /g, '20%');
-    console.log(formatTextMessage)
+
     return (
         <>
-            <div className="fixed bottom-2 left-5 flex flexColumn ph5">
+            <div className={`fixed bottom-2 left-5 flex flexColumn ph5 z-5 items-center `}>
                 <a
-                    href={`https://wa.me/${phone}?text=I'm%20interested%20in%20your%20car%20for%20sale`}
+                    href={`https://wa.me/${phone}?text=${formatTextMessage}`}
                     target='_blank'
                     rel="noreferrer noopener"
                     className={`bg-white br-50 flex justify-center items-center ph3 pv3`}
+                    style={{ borderRadius: "50%", height: "50px" }}
                 >
                     <img src={logo} width={width} height={height} alt="Logo" />
 
                 </a>
-                <p className="no-underline bg-white br2 ph3 pv3 mh2">Chatea con nosotros</p>
+                <p className={`no-underline bg-white br2 ph3 pv3 mh2 ${font}`}>Chatea con nosotros</p>
             </div>
         </>
     )
@@ -37,6 +40,7 @@ WhatsappButton.propTypes = {
     message: PropTypes.string,
     height: PropTypes.number,
     width: PropTypes.number,
+    font: PropTypes.string
 }
 
 WhatsappButton.defaultProps = {
@@ -44,7 +48,8 @@ WhatsappButton.defaultProps = {
     phone: "3208041066",
     message: "Estás comunicándote con Julio Colombia, por favor ingresa tu duda.",
     width: 35,
-    height: 35
+    height: 35,
+    font: "t-body"
 }
 
 WhatsappButton.schema = {
@@ -79,6 +84,11 @@ WhatsappButton.schema = {
             title: 'Alto',
             description: 'Agrega por favor el numero de telefono',
             type: 'number'
+        },
+        font: {
+            title: 'Fuente',
+            description: 'Agrega por la fuente del texto',
+            type: 'string'
         },
 
     }
